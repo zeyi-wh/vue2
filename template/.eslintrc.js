45c60673da8918a2f862b859d50360a9fa455a1f
付琦 <qi.fu@medcloud.cn>
2021-08-09 11:09:13 +0800

    feat: 修改eslint的配置

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,13 +2,29 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/standard'
+  ],
+  globals: {
+    'Vue': true,
+    'VueRouter': true,
+    'Vuex': true,
+    'axios': true,
+    'antd': true,
+    '_': true,
+    'moment': true
+  },
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-empty': 'off',
+    'prefer-promise-reject-errors': 'off'
+  }
+}
